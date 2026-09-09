304e1427d5106124efa3708b082c329941c279d3
Florian Kappert <florian.kappert@bilendo.de>
2026-09-09 15:52:37 +0200

    glowed v0.2.2-floriankappert.13

--- Formula/glowed.rb
+++ Formula/glowed.rb
@@ -1,9 +1,9 @@
 class Glowed < Formula
   desc "Ghostty-oriented terminal TUI Markdown browser/editor"
   homepage "https://github.com/floriankappert/glowed.md"
-  url "https://github.com/floriankappert/glowed.md/archive/refs/tags/v0.2.2-floriankappert.12.tar.gz"
-  version "0.2.2-floriankappert.12"
-  sha256 "2da95a1caa38a8b8040a249449b5de4508b41056aac5db7df52b68e14432de04"
+  url "https://github.com/floriankappert/glowed.md/archive/refs/tags/v0.2.2-floriankappert.13.tar.gz"
+  version "0.2.2-floriankappert.13"
+  sha256 "31df71e3edb816e555ad87d7e9780467dcdf2b28b623017a7090642328621d87"
   license "MIT"
 
   depends_on "go" => :build
